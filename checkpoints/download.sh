8349e3e0c36e077e80f5c379c94478a73c581d75
Hallee Wong <hallee@mit.edu>
2025-09-24 17:11:53 -0400

    fix download script and threshold logits at 0

--- checkpoints/download.sh
+++ checkpoints/download.sh
@@ -1,2 +1,2 @@
-wget -O ScribblePrompt_unet_v1_nf192_res128.pt https://www.dropbox.com/scl/fi/pnw88n05irnv5z1snlklr/ScribblePrompt_unet_v1_nf192_res128.pt
-wget -O MultiverSeg_v1_nf256_res128.pt https://www.dropbox.com/scl/fi/5sup9s5l9xkp28wbwkm9g/MultiverSeg_v1_nf256_res128.pt
+wget "https://www.dropbox.com/scl/fi/pnw88n05irnv5z1snlklr/ScribblePrompt_unet_v1_nf192_res128.pt?rlkey=dr8xvkf0wj2r082h1zzpcmz5o&dl=1" -O ScribblePrompt_unet_v1_nf192_res128.pt
+wget "https://www.dropbox.com/scl/fi/pcf7kgz87bqfrathbri63/MultiverSeg_v1_nf256_res128.pt?rlkey=j646jh2b1wocvptpeascrzrt0&dl=1" -O MultiverSeg_v1_nf256_res128.pt
